7f0d3f7bd24d3847b2454bd4132314910ec559f6
Aaron Trevino <Aaron@Aarons-MacBook-Pro.local>
2018-06-13 09:31:48 -0500

    Update

---
+++ JS/external.js
@@ -0,0 +1,55 @@
+// console.log("Hello from external javascript");
+//
+// alert("Welcome to my website!");
+//
+// var userInput = prompt("What is your favorite color?");
+// alert(("Great, ") + userInput + (" is my favorite color too!"))
+//
+// var pricePerDAy = 3
+// alert(("The cost for each rental per day wil be ") + ("$") + pricePerDAy)
+// var littleMermaidDaysRented = prompt("how many days will you rent The Little Mermaid?");
+// var brotherBearDaysRented = prompt("how many days will you rent The Brother Bear?");
+// var HerculesDaysRented = prompt("how many days will you rent The Hercules?");
+//
+// var totalPrice = (parseInt(littleMermaidDaysRented) + parseInt(brotherBearDaysRented) + parseInt(HerculesDaysRented)) * pricePerDAy;
+//
+// alert("Your total price will be " + "$" + totalPrice);
+//
+//
+// var googleHourlyPay = 400
+// var amazonHourlyPay = 380
+// var facebookHourlyPay = 350
+//
+// var googleHoursWorked = prompt("How many hours have you worked at Google this week?");
+// var amazonHoursWorked = prompt("How many hours have you worked at Amazon this week?");
+// var facebookHoursWorked = prompt("How many hours have you worked at Facebook this week?");
+//
+// var totalPay = parseInt(googleHoursWorked*googleHourlyPay) + parseInt(amazonHoursWorked*amazonHourlyPay) + parseInt(facebookHoursWorked*facebookHourlyPay);
+
+// alert("Your total pay for this week is " + "$" + totalPay);
+//
+// var classNotFull = confirm("Does the class have space?")
+// var noScheduleConflict = confirm("Does this class work with your schedule?")
+//
+// var canEnroll = noScheduleConflict && classNotFull;
+//
+// alert("it is " + canEnroll + " that you can enroll (lol)");
+
+// var numberOfItemsForOffer = 2;
+// var numberOfItemsPurchased = prompt("How many items have you purchased?");
+// var offerNotExpired = confirm("Is the offer still valid?");
+// var premiumMemberStatus = confirm("Are you a Premium Member?");
+//
+// var offerCanBeApplied = ((numberOfItemsPurchased >= numberOfItemsForOffer) && offerNotExpired) || premiumMemberStatus && offerNotExpired ;
+//
+// alert("It is " + offerCanBeApplied + " that this offer can be applied");
+
+var username = "codeup";
+var password = "notastrongpassword";
+
+var acceptablePasswordLength = password.length => 5;
+var acceptableUsernameLength = 20;
+
+var passwordDoesNotIncludeUsername =;
+var passwordTrim = ;
+var usernameTrim = ;
