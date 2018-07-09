7ea4ac3d71133d05e98d899146e32ff212b94d3f
Aaron Trevino <Aaron@Aarons-MacBook-Pro.local>
2018-07-09 10:57:10 -0500

    added pre-ajax files

---
+++ pre-ajax.js
@@ -0,0 +1,49 @@
+"use strict;"
+
+$(document).ready(function() {
+
+    var people = [
+        {
+            name: "Fred",
+            favColor: "red"
+        },
+        {
+            name: "Sally",
+            favColor: "blue"
+        },
+        {
+            name: "Tom",
+            favColor: "yellow"
+        }
+    ];
+
+    $('#add-names-btn').click(function () {
+        $('#names-list').append('<ul>');
+        people.forEach(function (person) {
+            $('#names-list').append('<li>' + person.name + '</li>');
+        });
+        $('#names-list').append('</ul>');
+    })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
